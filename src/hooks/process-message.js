ec28870910cb6f03e83c1b6120ecbcd8d6bb4def
Leo Nascimento <95266043+leo-sn@users.noreply.github.com>
2022-12-26 17:56:00 -0800

    Create hooks for adding users and process income message

---
+++ src/hooks/process-message.js
@@ -0,0 +1,27 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+// eslint-disable-next-line no-unused-vars
+module.exports = (options = {}) => {
+  return async context => {
+
+    const { data } = context;
+
+    if(!data.text) {
+      //Throw error jumps out of all hooks and dont trigger next ones if it is thrown
+      throw new Error('A message must have text')
+    }
+
+    const { user } = context.params;
+
+    const text = data.text.substring(0, 400);
+
+    context.data = {
+      text,
+      userId: user._id,
+      createdAt: new Date().getTime(),
+    }
+
+    return context;
+  };
+};
